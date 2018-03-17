2ecdf0af38e78312d5845398a0715676eaa9fe89
Johnny Lee-Othon <jleeothon@icloud.com>
2018-03-18 00:51:02 +0100

    Add chachalaca 0.1.1

--- packages/chachalaca-ui/routes/index.js
+++ packages/chachalaca-ui/routes/index.js
@@ -1,22 +1,38 @@
 const fs = require('fs')
+const tmp = require('tmp')
 
+const Lazy = require('lazy.js')
 const express = require('express');
 const multer = require('multer')
+const glob = require('glob')
+const parseInvoice = require('chachalaca').parse
+const rowify = require('chachalaca').rowify
+const generateCsv = require('chachalaca').generateCsv
 
-const upload = multer({dest: '/tmp/ortalis'})
+const upload = multer({dest: '/tmp'})
 
 const router = express.Router();
+
+const getFileContents = function * (path) {
+  yield fs.readFileSync(p, {flag: 'r'})
+}
 
 router.get('/', (req, res) => {
   res.render('index', {title: 'Ortalis'});
 });
 
 router.post('/generateCsv', upload.array('files'), (req, res) => {
-  const filePaths = _.map(req.files, file => file.path)
-  const contents = _.map(filePaths, p = fs.readFileSync(p, {flag: 'r'}))
+  const xmlFiles = Lazy(req.files).filter(f => f.originalname.endsWith('.xml'))
+  const filePaths = xmlFiles.map(f => f.path)
+  const fileContents = filePaths.map(f => fs.readFileSync(f, {encoding: 'UTF-8', flag: 'r'}))
+  const parsedObjects = fileContents.map(f => parseInvoice(f))
+  const rows = parsedObjects.map(r => rowify(r))
+  const rows = parsedObjects.map(r => rowify(r))
 
 
-  res.send(req.files[0])
+  const result = rowArrays.map(row => row.join(',')).join('\n')
+
+  res.send(result)
   // res.download(req.files[0].path, 'filename.xml')
 });
 
