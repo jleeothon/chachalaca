626918719e90a8ad674a59dc1a5f53d7bcb32f4e
Johnny Lee-Othon <jleeothon@icloud.com>
2018-10-21 17:24:06 +0200

    Refactor index.js

--- packages/chachalaca-ui/public/javascripts/index.js
+++ packages/chachalaca-ui/public/javascripts/index.js
@@ -1,7 +1,7 @@
 document.addEventListener(
 	'DOMContentLoaded',
 	() => {
-		const doStuff = function(_) {
+		function fileInputOnChange() {
 			const formData = new FormData();
 			const fileInput = document.getElementById('file-input');
 			for (const f of fileInput.files) {
@@ -10,25 +10,27 @@ document.addEventListener(
 
 			const request = new XMLHttpRequest();
 			request.responseType = 'blob';
-			request.addEventListener('load', function(_) {
-				if (this.status === 200) {
-					const a = document.createElement('a');
-					a.style = 'display: none';
-					document.body.appendChild(a);
-					const url = window.URL.createObjectURL(this.response);
-					a.href = url;
-					const date = new Date().toLocaleString().replace(/[^\d]/g, '-');
-					a.download = `facturas-${date}.xlsx`;
-					a.click();
-					window.URL.revokeObjectURL(url);
-				}
-			});
+			request.addEventListener('load', requestOnLoad);
 			request.open('POST', '/generate-xlsx');
 			request.send(formData);
-		};
+		}
+
+		function requestOnLoad() {
+			if (this.status === 200) {
+				const a = document.createElement('a');
+				a.style = 'display: none';
+				document.body.appendChild(a);
+				const url = window.URL.createObjectURL(this.response);
+				a.href = url;
+				const date = new Date().toLocaleString().replace(/[^\d]/g, '-');
+				a.download = `facturas-${date}.xlsx`;
+				a.click();
+				window.URL.revokeObjectURL(url);
+			}
+		}
 
 		const fileInput = document.getElementById('file-input');
-		fileInput.addEventListener('change', doStuff);
+		fileInput.addEventListener('change', fileInputOnChange);
 	},
 	false
 );
