faa86d417bca364f4d3d1ca8233450779e0c70e8
Johnny Lee-Othon <jleeothon@icloud.com>
2021-03-05 21:46:09 +0100

    Upload files to S3

--- packages/ui/routes/index.js
+++ packages/ui/routes/index.js
@@ -8,15 +8,23 @@ const multer = require('multer');
 const pMap = require('p-map');
 const formatISO = require('date-fns/formatISO');
 
-const spacesEndpoint = new AWS.Endpoint('fra1.digitaloceanspaces.com');
+const log = bunyan.createLogger({name: 'chachalaca'});
+
+const s3Endpoint = process.env.S3_ENDPOINT;
+const accessKeyId = process.env.S3_ACCESS_KEY_ID;
+const secretAccessKey = process.env.S3_SECRET_ACCESS_KEY;
+
+const spacesEndpoint = new AWS.Endpoint(s3Endpoint);
+if (!accessKeyId || !secretAccessKey) {
+	log.error('Did not find credentials for S3');
+	throw new Error('Did not find credentials for S3');
+}
 
 const s3 = new AWS.S3({
 	endpoint: spacesEndpoint,
-	accessKeyId: '...',
-	secretAccessKey: '...'
+	accessKeyId,
+	secretAccessKey
 });
-
-const log = bunyan.createLogger({name: 'chachalaca'});
 
 const chachalacaCore = require('@jleeothon/chachalaca-core');
 
@@ -52,22 +60,23 @@ router.post(
 );
 
 async function uploadFiles(filePaths) {
-	const date = formatISO(new Date()).replace(/\D/g, '-').slice(0, -1);
+	const dateTime = formatISO(new Date()).replace(/\D/g, '-').slice(0, -1);
+	const date = formatISO(new Date(), {representation: 'date'});
 	const {length} = filePaths.length.toString();
 
 	const fileInfos = filePaths.map((filePath, index) => {
 		const paddedIndex = (index + 1).toString().padStart(length, '0');
-		const name = `${date} - ${paddedIndex}.xml`;
-		return {name, path: filePath};
+		const key = `${date}/${dateTime} - ${paddedIndex}.xml`;
+		return {key, path: filePath};
 	});
 
 	return pMap(fileInfos, (fileInfo) => uploadFile(fileInfo), {concurrency: 3});
 }
 
-async function uploadFile({name, path}) {
+async function uploadFile({key, path}) {
 	const parameters = {
 		Bucket: 'chachalaca-space-1',
-		Key: name,
+		Key: key,
 		Body: readFileSync(path),
 		ACL: 'private'
 	};
